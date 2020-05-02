563d44e92bdc3b7b12e01f76cc2f2515c2215b9d
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-02 14:05:46 -0400

    chore: add review of notes

---
+++ Java.md
@@ -0,0 +1,85 @@
+# Java:
+
+## Main Class: 
+
+_In every Java application there is an entry point class usually called Main._ 
+
+_The class must match the name of the file in the directory._ 
+
+## Key Terms for Main Method :
+  _These terms make-up the main method signature._
+
+ - **Public** : This the access modifier to the main method. It has to be public so that Java runtime can execute this method.
+ - **Static** : The main method has to static so that JVM cab load the class into memory and call the main method.
+ - **Void** : Java main method doesn't return anything. 
+ - **String[] args** : The main method single argument of type String array. Also called the Command-Line arguments.
+
+## Key Terms: 
+  - **int** : interger
+  - **javac** : VS code CLI command for ava Complier
+
+
+_Java has 8 primitive and Wrapper data types
+Java has server data types to represent numbers to accommodate for optimization in memory allocation:_
+
+  - **primitive / Wrapper**
+  - byte   / Byte  (8-bit)
+  - short / Short   (16-bit)
+  - int / Integer (32-bit)
+  - long / Long    (64-bit)
+  - float / Float
+  - double / Double
+  
+_A double is used for precision of an accurate decimal value_
+
+  - boolean / Boolean (1-bit)
+  - char / Character (single quotes only)
+  - String (double quotes only)
+
+## Control Flow
+  - if, else if and else
+  - Switch
+  - For Loop 
+  - While Loop
+  - Do While Loop
+
+  - An Increment/decrement operator that is after a variable will increment/decrement after that line of code.
+  - An Increment/decrement operator that is before a variable will increment/decrement on when the line of code is run. 
+
+## Static method: 
+  _A method on class that has static  keyword can only be accessed on the class._
+  
+
+
+  ```
+    Class Main { 
+      public static void main(String[] args) {
+        add();
+      }
+
+      public static int staticAdd(int a, int b) {
+        return a + b
+      }
+    }
+  ```
+
+  _A method without the static keyword can be access on the instance  of object of that class._
+
+  ```
+    Class Main { 
+      public static void main(String[] args) {
+        <!-- Create an instance of -->
+        Main main = new Main();
+        <!-- Invoke the main method on that instance -->
+        main.add();
+      }
+
+      public int Add(int a, int b) {
+        return a + b
+      }
+    }
+  ```
+
+# Sources:
+
+- [What is the Command-Line arguments?](https://docs.oracle.com/javase/tutorial/essential/environment/cmdLineArgs.html) 
--- Main.java
+++ Main.java
@@ -1,3 +1,119 @@
-public class Main {
-    
+// Class must match the name of the file
+// Root
+// Strings[ ] string in array of argumenet
+
+// int - interger
+
+// Javac - Java Complier
+class Main { 
+  //Entry Point of the Application
+  // Public is an Access modifer method can be used globaly
+  // Static is class method rather than instance menthod 
+  //  return type Void is for a method that returns nothing. 
+  // Method Signuture
+  public static void main(String[] args) {
+    // Main myMain = new Main();
+    Main main = new Main();
+    // System.out.println("Hello World");
+    // add();
+    // Method on the class
+    System.out.println(staticAdd());
+    // Method on the instance
+    System.out.println(main.add(2, 3));
+
+    // Refering to how man bits for memory allocate
+    // Optimize for scalabilty
+    // Can store 8-bit -128 to 127;
+    // 8 primitives 8 Wrappers data types
+
+    // Primative Data type
+    byte myByte = 1;
+    // Has access to methods from number class. 
+    // Class Data type
+    Byte myWrapperByte = 1; 
+    // Can store 16-bit up 3200 +/-
+    short myShort = 1;
+    Short myWrapperShort = 1;
+    // Can sotre 32-bit about 2 billion.
+    int myVar = 3;    
+    // Interger myWrapperVar = 5;
+    // Can store 64-bit up ...
+    long myLong = 123456;
+    Long myWrapperLong = 12345l;
+
+
+
+    float myFloat = 2.5f;
+    Float myWrapperFloat = 2.5f;
+    // ....alot for percision
+    double myDouble = 2.34567;
+    Double myWrapperDouble = 2.3456;
+
+    // 1-bit  0 - 1;
+    boolean foo = false;
+    Boolean bar = true;
+
+    char myChar = 'a';
+    Character myWrapperChar = 'B';
+
+    String myString = "Test";
+
+    // ==== CONTROL FLOW ====\\
+
+    if( bar == true)  { 
+      System.out.println("True"); 
+    }
+    // Strings have to be double quotes.
+    else if(!bar) {
+      System.out.println("False");
+    }
+    else { 
+      System.out.println("Something Else");
+    }
+
+    // myVarr++ Adding it after the line is executed
+    // ++myVar Adding it right when the line is executed. 
+
+    if(myVar == 5) {
+      myVar++;
+    }
+    else if(myVar == 6) {
+      myVar--;
+    }
+    else { 
+      myVar -= 3; 
+    }
+
+    for(int i = 0; i < myVar; i++) {
+      System.out.println(i);
+    }
+
+    while(myVar < 10) {
+      myVar++; 
+    }
+
+    // run at atlest once
+    do {
+      myVar++;
+    }while(myVar < 10);
+
+    switch(myString) {
+      case "Test":
+        System.out.println("Its Working");
+        break;
+      case " Error":
+        System.out.println("It broke!");
+        break;
+      default: 
+        break;
+    }
+  }
+  public static int staticAdd() {
+    int a = 5; 
+    int b = 3; 
+    return a + b; 
+  }
+  private int add(int a, int b) {
+    return a + b; 
+  }
 }
