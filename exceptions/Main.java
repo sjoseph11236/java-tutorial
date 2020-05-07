8349c7070f1ca0f56784cfd3a560d59fbf81db39
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-07 17:43:43 -0400

    chore: add notes on exceptions

---
+++ exceptions/Main.java
@@ -0,0 +1,30 @@
+class Main { 
+  public static void main(String[] args) throws ArithmeticException {
+    
+    // int inty = 10 / 0; 
+
+    // System.out.println("After exception");
+    // int x;
+    
+    // try {
+    //   x = 10 / 0; 
+    // } catch (ArithmeticException ae) {
+    //   System.out.println("Can't divide by zero");
+    //   x = 0; 
+    // }
+    // finally {
+    //   System.out.println("I am here at finally");
+    // }
+
+    // System.out.println(x);
+
+    // command to manuel create a exception
+    // throw 
+    // //  your expecting a n error that needs tob ehandle in other needs to be handled somewhere. 
+
+        // throws
+  }
+}
+
+// fn + F5 is run 
+// sysout for System.out.println()
---
+++ exceptions/exceptions.md
@@ -0,0 +1,12 @@
+# Exceptions 
+
+An exception is different from errors.
+
+An exception
+
+
+Recovering from Error is not possible. 
+ exceptions are code related 
+ errors are external
+
+ exceptions happen at runtime
